2c4aa88f09c7d36a608ebc4c5cf1692b3a541c6a
raajrajnish <raajrajnish@gmail.com>
2023-06-19 16:48:11 +0530

    added html parser class

---
+++ simplified/simplifiedai/HTMLParser.py
@@ -0,0 +1,27 @@
+class HtmlParser:
+
+    def type_input_box(self):
+        return [
+            'type="button"',
+            'type="checkbox"',
+            'type="email"',
+            'type="file"',
+            'type="image"',
+            'type="number"',
+            'type="password"',
+            'type="radio"',
+            'type="submit"',
+            'type="text"',
+            # 'type = "color"',
+            # 'type = "date" ',
+            # 'type = "datetime-local" ',
+            # 'type = "hidden"',
+            # 'type = "month"',
+            # 'type = "range"',
+            # 'type = "reset"',
+            # 'type = "search"',
+            # 'type = "tel"',
+            # 'type = "time"',
+            # 'type = "url"',
+            # 'type = "week"',
+        ]
--- simplified/simplifiedai/IdentifyLocators.py
+++ simplified/simplifiedai/IdentifyLocators.py
@@ -1,4 +1,12 @@
 from bs4 import BeautifulSoup
+from .HTMLParser import *
+
+
+def createXpath(tags):
+    xpath = ['//*[@' + key + "='" + ' '.join(value) + "']" if type(value) is list else
+             '//*[@' + key + "='" + value + "']" for key, value in tags.attrs.items()]
+    # [print(path) for path in xpath]
+    return xpath
 
 
 class ParseDOM:
@@ -10,9 +18,24 @@ class ParseDOM:
             tag = doc.find_all('input')
             for tags in tag:
                 if 'hidden' not in str(tags).lower():
-                    xpath = ['//*[@' + key + "='" + ' '.join(value) + "']" if type(value) is list else
-                          '//*[@' + key + "='" + value + "']" for key, value in tags.attrs.items()]
-                    all_paths.append(xpath)
+                    print(f'***** Html Tag *****')
+                    # identifiers = ['text','password']
+                    print(f'{tags}')
+                    html_tag_identified = [html_tags for html_tags in HtmlParser().type_input_box() if
+                                           html_tags in str(tags).lower()]
+                    if 'password' in str(html_tag_identified[0]).lower():
+                        print(f'***** Xpath Identified for Password Field *****')
+                        xpath = createXpath(tags=tags)
+                        all_paths.append(xpath)
+                    elif 'text' in str(html_tag_identified[0]).lower():
+                        print(f'***** Xpath Identified for Text Field *****')
+                        xpath = createXpath(tags=tags)
+                        all_paths.append(xpath)
+                    elif 'submit' in str(html_tag_identified[0]).lower():
+                        print(f'***** Xpath Identified for Submit Form *****')
+                        xpath = createXpath(tags=tags)
+                        all_paths.append(xpath)
+
             return all_paths
         except:
             raise Exception('ERROR - not able to generate the xpath!')
